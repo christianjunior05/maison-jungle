155b00ac59db54721e5c8b10c1f67a9f901bf702
christianjunior05 <bchrist.junior@gmail.com>
2024-09-13 23:37:19 +0000

    addd

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,7 @@ import logo from '../styles/Banner.css'
 import '../assets/logo.png'
 function Banner() {
     const title ="la maison jungle "
+    
 return(
     <div className='lmj-banner' >
         <img src={logo} alt='La maison jungle' className='lmj-logo' />
