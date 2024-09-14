6c2b5e30489c7ed5d9e4c528714d71c81ae5a481
christianjunior05 <bchrist.junior@gmail.com>
2024-09-14 21:05:19 +0000

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import Banner from "./Banner";
-import Cart from './Cart';
+
 
 
 function App() {
   return (
   <div>
     <Banner/>
-    <Cart/>
+   
   </div>) 
 
 }  
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,15 @@
-import logo from '../styles/Banner.css'
-import '../assets/logo.png'
+import '../styles/Banner.css';  // Import correct du fichier CSS
+import logo from '../assets/logo.png';  // Import correct de l'image
+
 function Banner() {
-    const title ="la maison jungle "
+    const title = "La maison jungle";  // Variable pour le titre
     
-return(
-    <div className='lmj-banner' >
-        <img src={logo} alt='La maison jungle' className='lmj-logo' />
-        <h1>{{title}}</h1>
-    </div>
-)
+    return (
+        <div className='lmj-banner'>
+            <img src={logo} alt='La maison jungle' className='lmj-logo' />  {/* Utilisation de l'image */}
+            <h1>{title}</h1>  {/* Affichage de la variable title sans {{}} */}
+        </div>
+    );
 }
 
 export default Banner;
-
