f1377cfa2227022ddd65c5ebb83678992fa2d607
christianjunior05 <bchrist.junior@gmail.com>
2024-09-14 21:17:24 +0000

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import Banner from "./Banner";
+import ShoppingList from "./ShoppingList";
 
 
 
@@ -6,7 +7,7 @@ function App() {
   return (
   <div>
     <Banner/>
-   
+   <ShoppingList/>
   </div>) 
 
 }  
