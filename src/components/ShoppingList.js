ecd5dfe5ff9419040b9e01e1c9d2ced1f154c7d1
christianjunior05 <bchrist.junior@gmail.com>
2024-09-30 23:04:06 +0000

    add

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -10,40 +10,18 @@ const plantList = [
         id: '1ed',
         isBestSale: true
     },
-    {
-        name: 'ficus lyrata',
-        category: 'classique',
-        id: '1ed',
-        isBestSale: true
-    },    {
-        name: 'pothos argenté',
-        category: 'classique',
-        id: '1ed',
-        isBestSale: true
-    },    {
-        name: 'yucca',
-        category: 'classique',
-        id: '1ed',
-        isBestSale: true
-    },    {
-        name: 'palmier',
-        category: 'classique',
-        id: '1ed',
-        isBestSale: true
-    },
-]
+];
 
 function ShoppingList() {
     return (
         <ul>
-        {plantList.map((plant) => (
-            <li key={ plant.id }>
-                {plant.name}
-                {plant.isBestSale && <span>🔥</span> }
-            </li>
-))}
+            {plantList.map((plant, index) => (
+                <li key={index}>
+                    {typeof plant === 'string' ? plant : plant.name}
+                </li>
+            ))}
         </ul>
-    )
+    );
 }
 
 export default ShoppingList;
